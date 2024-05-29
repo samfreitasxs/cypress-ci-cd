e003361e6eeac08497111b8c38a0dc77a8c1455b
samfreitasxs <samuel.sfdf@gmail.com>
2024-05-29 17:18:44 -0300

    Inlcusao de novos testes

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     env: {
-      API_URL: 'https://api.coursify.me/v1',
+      API_URL: 'https://portal-api-p1-dev.pmerj.seg.br/',
     },
   },
   fixturesFolder: false,
